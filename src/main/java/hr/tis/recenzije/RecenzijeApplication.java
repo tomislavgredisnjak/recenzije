348bd35bfdc755e43bd4e96aff506e0e566e6b43
tomislav <tgtomislav.gredisnjak@gmail.com>
2025-04-07 17:04:53 +0200

    initial commit

---
+++ src/main/java/hr/tis/recenzije/RecenzijeApplication.java
@@ -0,0 +1,13 @@
+package hr.tis.recenzije;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecenzijeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecenzijeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/tis/recenzije/RecenzijeApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tis.recenzije;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecenzijeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
